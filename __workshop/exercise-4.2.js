ca549840a720458cb5f4837908a9024e21989f61
myriam mansouri <myriam.mansouri@gmail.com>
2020-07-13 13:37:03 -0400

    Exercise 4.2

--- __workshop/exercise-4.2.js
+++ __workshop/exercise-4.2.js
@@ -1,5 +1,19 @@
-const request = require('request-promise');
+const request = require("request-promise");
 
 const getTronaldDumpQuote = async () => {
-  // write write write
+  try {
+    const joke = await request({
+      uri: `https://api.tronalddump.io/random/quote`,
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    return JSON.parse(joke).value;
+  } catch (err) {
+    return err;
+  }
 };
+
+getTronaldDumpQuote()
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));

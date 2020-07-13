d0ed7777f18cbd9a69c84a0ac1753beca57c2916
myriam mansouri <myriam.mansouri@gmail.com>
2020-07-13 13:36:54 -0400

    Exercise 4.1

--- __workshop/exercise-4.1.js
+++ __workshop/exercise-4.1.js
@@ -1,6 +1,19 @@
-const request = require('request-promise');
+const request = require("request-promise");
 
 // getDadJoke
-
+const getDadJoke = async () => {
+  try {
+    const joke = await request({
+      uri: `https://icanhazdadjoke.com/`,
+      headers: { Accept: "application/json" },
+    });
+    const parseJoke = JSON.parse(joke);
+    return parseJoke;
+  } catch (err) {
+    return err;
+  }
+};
 // 4.1
-// getJoke().then((data) => console.log(data));
+getDadJoke()
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
